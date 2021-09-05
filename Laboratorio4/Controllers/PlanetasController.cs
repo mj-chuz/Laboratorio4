f811e761aff469e21b9579adb01e016cb6f8af85
mj-chuz <medranom778@gmail.com>
2021-09-04 21:48:22 -0600

    Laboratorio 4.1, Tabla Planetas y proyecto mvc

---
+++ Laboratorio4/Controllers/PlanetasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio4.Models;
+using Laboratorio4.Handlers;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        // GET: Planetas
+        public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
+            return View();
+        }
+       
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Planeta(
+	planetaId		INTEGER PRIMARY KEY IDENTITY, --identity es para que se incremente automAticamente 1,2,3...
+	nombrePlaneta	NVARCHAR(MAX) NOT NULL,
+	numeroAnillos	INTEGER NOT NULL,
+	tipoPlaneta		NVARCHAR(100) NOT NULL,
+	archivoPlaneta	VARBINARY(MAX),
+	tipoArchivo		NVARCHAR(50) --esto es solo para guardar la extensiOn del archivo (pdf, png...)
+)
